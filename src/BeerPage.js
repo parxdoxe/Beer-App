8ac59bf9916155db2617ad9bdfcde2cf0d9a8f17
parxdoxe <dumoulin.florian22@gmail.com>
2022-06-14 10:40:15 +0200

    beerPage

--- src/BeerPage.js
+++ src/BeerPage.js
@@ -2,27 +2,39 @@ import React from "react";
 import { withRouter } from './index';
 import axios from "axios"
 import Header from "./Header";
+import Loader from "./Loader"
 
 class BeerPage extends React.Component{
     constructor(props){
         super(props)
         this.state= {
-            selected: {},
+            selected: null,
+            loading: true,
         }
       }
 
-      componentDidMount(){
+    componentDidMount(){
         this.getBeer(this.props.router.params.id)
+        this.setState({ loading:false })
       }
       
     getBeer = (id) => {
         axios.get(`https://api.punkapi.com/v2/beers/${id}`).then(response => {
-            this.setState({ selected: response.data})
+            this.setState({ selected: response.data[0] })
         })
     }
 
 
     render(){
+
+        if (this.state.loading) {
+            return <Loader />
+        }
+
+        let select = null;
+        if (this.state.selected) {
+            select = <h1>{this.state.selected.name}</h1>
+        }
         
         return (
 
@@ -33,9 +45,7 @@ class BeerPage extends React.Component{
                         <Header name="Beer App !" />
                     </div>
                     <div className="beer">
-                    <h1>{this.props.router.params.name}</h1>
-               
-                    {this.state.selected.name} 
+                    {select}  
                     </div>
                 </div>
             
---
+++ src/Loader.js
@@ -0,0 +1,5 @@
+function Loader (){
+    return <p>Chargement en cours</p>
+    }
+    
+    export default Loader
